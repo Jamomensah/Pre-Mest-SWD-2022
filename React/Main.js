428b8f3a55cb4639a38108e512933f316e7a9d6c
Jennifer Amo-Mensah <jenyamomensah@gmail.com>
2022-08-16 10:42:51 +0000

    Added Main and Nav components to react folder

---
+++ React/Main.js
@@ -0,0 +1,11 @@
+import Heading from "./Heading";
+import Lists from "./Lists"
+function Main(){
+    return(
+        <div className="main">
+          <Heading/>
+          <Lists/>
+        </div>
+    )
+}
+export default Main;

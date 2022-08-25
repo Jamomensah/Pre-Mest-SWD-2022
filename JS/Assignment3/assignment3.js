0326b1e9b91eccedd1c3e8995fa67f3fd82ef574
Jennifer Amo-Mensah <jenyamomensah@gmail.com>
2022-08-25 22:18:30 +0000

    added assignment 3

--- JS/Assignment3/assignment3.js
+++ JS/Assignment3/assignment3.js
@@ -1,1 +1,49 @@
+const h2 = document.querySelector('h2');
+
+const increase = document.querySelector('.increase');
+let value = 0;
+
+
+increase.addEventListener('click', ()=> {
+    value += 1;
+    h2.textContent = value;
+    h2.style.color = 'green'
+}
+)
+
+const decrease= document.querySelector('.decrease')
+decrease.addEventListener('click', ()=> {
+    value -= 1;
+    h2.textContent = value;
+    h2.style.color = 'red'
+}
+)
+
+const reset= document.querySelector('.reset')
+reset.addEventListener('click', ()=> {
+    value = 0;
+    h2.textContent = 0;
+    h2.style.color = 'black'
+}
+)
+
+
+// const increase=document.querySelector('increase')
+// const decrease= document.querySelector('decrease')
+// const reset= document.querySelector('reset')
+// let value=i 
+// let i=0
+
+// for(i=0; i+=1; i++){
+//    alert(i);
+// } elseif(i-=1; i--){
+//     alert(i);
+// }else{
+//     alert(i=0);
+// }
+
+
+
+
+
 
